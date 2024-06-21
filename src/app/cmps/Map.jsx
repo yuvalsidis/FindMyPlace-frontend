956eaab825187137a2bb0752b19e15e7a0affcc2
yuvalsidis <yuvalsidis@gmail.com>
2024-06-21 22:43:58 +0300

    Change defaultCenter props

--- src/app/cmps/Map.jsx
+++ src/app/cmps/Map.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 import styles from '../styles/cmps/Map.module.scss';
+import MarkerComponent from "./MarkerComponent";
 
-const MapComponent = ({location}) => {
-    const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+const MapComponent = ({ location }) => {
+  const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
-    if (!API_KEY) {
-        return <h1>Loading</h1>;
-    }
-    
-    return (
-      <APIProvider apiKey={API_KEY}>
+  if (!API_KEY) {
+    return <h1>Loading</h1>;
+  }
+
+  return (
+    <APIProvider apiKey={API_KEY}>
       <Map className={styles.map}
-        defaultCenter={!location?{lat: 22.14992, lng: 0} : {lat: location.latitude, lng: location.longitude}}
+        defaultCenter={location}
         defaultZoom={5}
         gestureHandling={'greedy'}
         disableDefaultUI={true}
       />
+      {location && <MarkerComponent location={location} />}
     </APIProvider>
-    );
+  );
 };
 
 export default MapComponent;
