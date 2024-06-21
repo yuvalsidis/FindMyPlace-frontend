8b2b7a34beaa3bb02e35b76834241def574ac876
yuvalsidis <yuvalsidis@gmail.com>
2024-06-21 20:11:58 +0300

    Fix handleOpenModal and handleCloseModal fnc names

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,32 @@
 'use client'
 import Image from "next/image";
 import MainContentWrapper from "./cmps/MainContentWrapper";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LocationAccessModal from "./cmps/LocationAccessModal";
 
 export default function Home() {
   const [location, setLocation] = useState(null)
-  console.log('The location is:', Location)
+  console.log('The location is:', location)
+
+  const [isLocationModalOpen, setIsLocationModalOpen] = useState(true)
+
+  const handleOpenModal = () => {
+    setIsLocationModalOpen(true);
+  }
+
+  const handleCloseModal = () => {
+    setIsLocationModalOpen(false);
+  }
 
   return (
     <>
-      <MainContentWrapper location={location}/> 
-      <LocationAccessModal setLocation={setLocation}/>
+      <MainContentWrapper location={location} />
+      <LocationAccessModal
+        isLocationModalOpen={isLocationModalOpen}
+        setLocation={setLocation}
+        handleOpenModal={handleOpenModal}
+        handleCloseModal={handleCloseModal}
+      />
     </>
-  );
+  )
 }
